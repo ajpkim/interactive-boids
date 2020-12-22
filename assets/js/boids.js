f611a7d79586fc0bb140f3aa0ebc653107f6208f
ajpkim <alexkim345@gmail.com>
2020-12-21 23:35:01 -0500

    fin. vector class, steering forces outline, simulation is choppy

--- assets/js/boids.js
+++ assets/js/boids.js
@@ -1,161 +1,266 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-
 const MAX_X = canvas.width;
 const MAX_Y = canvas.height;
-let speedLimit = 5;
 
-////////////////////////////////
+let maxPerceptionRange = 75;
+let speedLimit = 3;
+let boidIdCount = 0;
 
+// Slider variables:
+let alignMagnitude = 0;
+let separateMagnitude = 100;
+let cohereMagnitude = 0;
 
+let alignRange = 75;
+let separateRange = 25;
+let cohereRange = 75;
+
+//////////////////////////////////////////////////
 function initSimulation () {
     console.log('Initializing simulation...');
 }
 
-////////////////////////////////////////
-// Helper functions
+//////////////////////////////////////////////////
+// Helper functions:
+
+// Return a random number within range [min, max].
 function randomNumber(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-function getDistance(boid, other) {
-    const dist = ((boid.x - other.x) ** 2) + ((boid.y - other.y) ** 2)
-    return Math.sqrt(dist)
+// Make boid position wrap around canvas instead of fly continually off-screen.
+function wrapEdges(boid) {
+    if (boid.position.x + boid.size < 0) {
+	boid.position.x = MAX_X;
+    } else if (boid.position.x - boid.size > MAX_X) {
+	boid.position.x = 0;
+    }
+    if (boid.position.y + boid.size < 0) {
+	boid.position.y = MAX_Y
+    } else if (boid.position.y - boid.size > MAX_Y) {
+	boid.position.y = 0;
+    }
 }
 
+//////////////////////////////////////////////////
+class Vector2d {
+    constructor(x, y) {
+	this.x = x;
+	this.y = y;
+    }
+    copy() { return new Vector2d(this.x, this.y) };
+    magnitude() { return Math.sqrt(this.x**2 + this.y**2) };
+    setMagnitude(mag) {
+	if (this.magnitude() === 0) {
+	    return this;
+	} else {
+	    this.div(this.magnitude());
+	    this.mult(mag);
+	    return this;
+	}
+    }
+    limitMagnitude() {
+	if (this.magnitude() > speedLimit) {
+	    this.setMagnitude(speedLimit)
+	}
+	return this;
+    }
+    
+    normalize() { return this.setMagnitude(1) };
 
-
-/////////////////////////////////////////
-// Boids
+    euclideanDistance(other) {
+	return Math.sqrt((this.x - other.x)**2 + (this.y - other.y)**2);
+    }
+    
+// Basic vector and scalar arithmetic operations.
+    add(other) {
+	if (other instanceof Vector2d) {
+	    this.x += other.x;
+	    this.y += other.y;
+	    return this;
+	} else if (typeof other === "number") {
+	    this.x += other;
+	    this.y += other;
+	    return this;
+	} else {
+	    throw new TypeError(`Invalid addition. ${other} is neither vector nor scalar`);
+	}
+    }
+    subtract(other) {
+	if (other instanceof Vector2d) {
+	    this.x -= other.x;
+	    this.y -= other.y;
+	    return this;
+	} else if (typeof other === "number") {
+	    this.x -= other;
+	    this.y -= other;
+	    return true;
+	} else {
+	    throw new TypeError(`Invalid subtraction. ${other} is neither vector nor scalar`);
+	}
+    }
+    // Scalar multipication.
+    mult(other) {
+	if (typeof other === "number") {
+	    this.x *= other;
+	    this.y *= other;
+	    return this;
+	} else {
+	    throw new TypeError(`Invalid scalar multiplication. ${other} is not a scalar`);
+	}
+    }
+    // Scalar division.
+    div(other) {
+	if (typeof other === "number") {
+	    this.x /= other;
+	    this.y /= other;
+	    return this;
+	} else {
+	    throw new TypeError(`Invalid scalar division. ${other} is not a scalar`);
+	}
+    }
+    
+    toString() {
+	return `<Vector2d obj with position (${this.x, this.y})>`;
+    }
+}
+//////////////////////////////////////////////////
 class Boid {
     constructor() {
 	this.id = boidIdCount++;
 	this.size = 5
-	this.x = randomNumber(0, MAX_X);
-	this.y = randomNumber(0, MAX_Y);
-	this.velocity = randomNumber(0, speedLimit);
-	this.acceleration = 0
+	this.position = new Vector2d(randomNumber(0, MAX_X), randomNumber(0, MAX_Y));
+	this.velocity = new Vector2d(randomNumber(-speedLimit, speedLimit), randomNumber(-speedLimit, speedLimit)).limitMagnitude(speedLimit);
+	this.acceleration = new Vector2d(0, 0);
 	this.flockmates = new Set()
     }
+   
+    getForceFlockmates() {
+	let forceFlockmates = {
+	    alignMates: [],
+	    cohereMates: [],
+	    separateMates: [],
+	}
+	for (let flockmate of this.flockmates) {
+	    let distance = boidDistances[this.id][flockmate.id];
+	    if (distance <= alignRange) forceFlockmates["alignMates"].push(flockmate);
+	    if (distance <= cohereRange) forceFlockmates["cohereMates"].push(flockmate);
+	    if (distance <= separateRange) forceFlockmates["separateMates"].push(flockmate);
+	}
+	return forceFlockmates;
+    }
 
-    // Make boids wrap canvas if position is off canvas
-    set x(xCoord) {
-	if (xCoord < 0) {
-	    this._x = MAX_X;
-	} else if (xCoord > MAX_X) {
-	    this._x = 0;
-	} else {
-	    this._x = xCoord;
+    // Calculate and apply the alignment, cohesion, and separation forcee to boid acceleration:
+    
+    // Flock centering.
+    align(alignMates) {
+	if (alignMates.length === 0) return;
+	let alignForce = new Vector2d(0, 0);
+	for (let other of alignMates) {
+	    alignForce.add(other.velocity);
+	};
+	alignForce.div(alignMates.length);
+	alignForce.setMagnitude(alignMagnitude);
+	this.acceleration.add(alignForce);
+    }
+    // Collision avoidance.
+    separate(separateMates) {
+	if (separateMates.length === 0) return;
+	let separateForce = new Vector2d(0, 0);
+	for (let other of separateMates) {
+	    let distance = boidDistances[this.id][other.id];
+	    let diffVector = this.position.copy().subtract(other.position);
+	    
+	    // Weight further away boids less for separation purposes.
+	    diffVector.div(distance);
+	    separateForce.add(diffVector.mult(-1));
 	}
+	separateForce.div(separateMates.length);
+	separateForce.setMagnitude(separateMagnitude);
+	this.acceleration.add(separateForce);
     }
-    get x() {
-	return this._x;
+    // Flock centering.
+    cohere(cohereMates) {
+	if (cohereMates.length === 0) return;
+	let cohereForce = new Vector2d(0, 0);
+	for (let other of cohereMates) {
+	    cohereForce.add(other.position);
+	};
+	cohereForce.div(cohereMates.length);
+	cohereForce.setMagnitude(cohereMagnitude);
+	this.acceleration.add(cohereForce);
     }
-    set y(yCoord) {
-	if (yCoord < 0) {
-	    this._y = MAX_Y;
-	} else if (yCoord > MAX_Y) {
-	    this._y = 0;
-	} else {
-	    this._y = yCoord;
-	}
+    
+    calcSteeringForces() {
+	let { alignMates, cohereMates, separateMates } = this.getForceFlockmates();
+	this.align(alignMates);
+	this.cohere(cohereMates);
+	this.separate(separateMates);
     }
-     get y() {
-	return this._y
+
+    updatePosition() {
+	this.calcSteeringForces();
+	this.velocity.add(this.acceleration)
+	this.velocity.limitMagnitude(speedLimit);
+	this.position.add(this.velocity);
+	this.acceleration.mult(0);
+	wrapEdges(this);
     }
 }
 
-
+//////////////////////////////////////////////////
+// Calculate and store the distance between each boid in a global 2d matrix and update each boids flockmates array.
 function calcDistancesFlockmates() {
     for (let [i, boid] of flock.entries()) {
 	boid.flockmates.clear();
-	// Q: is slicing here bad bc of copy creations??
-	// for (let other of flock.slice(i)) {
+	// Only iterate through each pair one time.
 	for (let j = i + 1; j < flock.length; j++) {
 	    other = flock[j]
-	    let dist = getDistance(boid, other);
-	    // Update global distance matrix
-	    boidDistances[boid.id][other.id] = dist;
-	    boidDistances[other.id][boid.id] = dist;
-	    // Update boid flockmates
-	    if (dist <= maxPerceptionRange) {
+	    let distance = boid.position.euclideanDistance(other.position);
+	    // Update global distance matrix.
+	    boidDistances[boid.id][other.id] = distance;
+	    boidDistances[other.id][boid.id] = distance;
+	    // Update flockmates for both boids.
+	    if (distance <= maxPerceptionRange) {
 		boid.flockmates.add(other);
 		other.flockmates.add(boid);
 	    }
 	}
     }
-}
-
-
-
-// function findFlockmates(boid, flock) {
-//     let flockmates = flock.filter(other => {
-// 	return getDistance(boid, other) <= boid.maxPerceptionRadius
-//     })
-//     return flockmates
-// }
-
-
-////////////////////////////////////////
-// Steering forces
-
-// Is it going to be inefficient to filter map for each force??
-
-function align(boid, other) {    
-}
-
-function separate(boid, other) {
-    
-}
-
-function cohere(boid, other) {
-}
-
-function calcSteeringForce(boid) {
-}
-
-////////////////////////////
+}    
+//////////////////////////////////////////////////
 // Animation
-
 function drawBoid(boid) {
-    ctx.moveTo(boid.x, boid.y);
-    ctx.arc(boid.x, boid.y, boid.size, 0, Math.PI * 2, false);
+    ctx.moveTo(boid.position.x, boid.position.y);
+    ctx.arc(boid.position.x, boid.position.y, boid.size, 0, Math.PI * 2, false);
     ctx.fill();
-//    ctx.stroke();  // What does this do? Doesn't appear necessary...
+    ctx.stroke();  // What does this do? Doesn't appear necessary...
 }
-
 
 function runSim() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
     flock.forEach((boid) => {
-	drawBoid(boid);
-	boid.x += boid.velocity;
-	boid.y += boid.velocity;
-    })    
+	drawBoid(boid);	
+	boid.updatePosition();
+    });    
     requestAnimationFrame(runSim);
 }
 
-//////////////////////////////////
-//// Test Run
-
-let maxPerceptionRange = 100;
-let boidIdCount = 0  // Class variables seem not standard in JS...??
-
+//////////////////////////////////////////////////
+// Test Run
 initSimulation();
 
 const flock = []
-for (let i=0; i < 10; i++) {
+for (let i=0; i < 20; i++) {
     flock.push(new Boid());
 }
 
-flock.forEach(boid => drawBoid(boid));
-
+// flock.forEach(boid => drawBoid(boid));
 
 let boidDistances = Array.from(Array(flock.length), () => new Array(flock.length))  // creates 2d array to hold boid distances i.e. [0][2] holds dist from boid 0 to boid 2
 
 calcDistancesFlockmates();
-
-
 runSim(flock);
